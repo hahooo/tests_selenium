bfc3fc72b087c1b3065d41f77658c98298458109
hahooo <hahoo.ru@yandex.ru>
2022-07-29 11:17:31 +0300

    Add Selenium Listener, update conftest.py

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -8,13 +8,5 @@ class TestHomepage:
 
     def test_nav_links(self):
         homepage_nav = HomepageNav(self.driver)
-        actual_links = homepage_nav.get_nav_links_text()
-        expected_links = homepage_nav.NAV_LINKS_TEXT
-        assert expected_links == actual_links, 'Validating Nav Links Text'
-        # homepage_nav.get_nav_link_by_name('Beauty').click()
-        # time.sleep(5)
         for index in range(12):
-            print('index', index)
             homepage_nav.get_nav_links()[index].click()
-            homepage_nav.driver.delete_all_cookies()
-            time.sleep(3)
